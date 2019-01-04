9ce179b72a67e13991495a46ebc6717083a5e98c
EgorShum <89135509130@mail.ru>
2019-01-04 17:44:35 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            REDIT <code>src/App.js</code> and save reload.
+            REDIT <code>src/App.js</code> I was changed in feature branch and save reload.
           </p>
           <a
             className="App-link"
